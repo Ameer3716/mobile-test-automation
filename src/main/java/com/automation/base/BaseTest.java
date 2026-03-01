55dfd1a68441cfc458394dd651461580457629a0
zaidkashif <zaid kashif996@gmail.com>
2026-03-01 19:33:13 +0500

    feat: implement BaseTest with driver setup and AppiumUtils - closes #3

---
+++ src/main/java/com/automation/base/BaseTest.java
@@ -0,0 +1,52 @@
+﻿package com.automation.base;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import com.automation.utils.AppiumUtils;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class BaseTest {
+
+    // This driver will be used by all test classes
+    protected AndroidDriver driver;
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        // Read from config.properties
+        String appiumUrl   = AppiumUtils.getProperty("appium.server.url");
+        String platformName = AppiumUtils.getProperty("platform.name");
+        String deviceName   = AppiumUtils.getProperty("device.name");
+        String appPath      = AppiumUtils.getProperty("app.path");
+        String automationName = AppiumUtils.getProperty("automation.name");
+
+        // Configure desired capabilities
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName(platformName);
+        options.setDeviceName(deviceName);
+        options.setApp(System.getProperty("user.dir") + "/" + appPath);
+        options.setAutomationName(automationName);
+        options.setNoReset(false);
+
+        // Start the driver
+        driver = new AndroidDriver(new URL(appiumUrl), options);
+
+        // Set implicit wait
+        int implicitWait = Integer.parseInt(AppiumUtils.getProperty("implicit.wait"));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWait));
+
+        System.out.println("Driver started successfully");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            System.out.println("Driver closed successfully");
+        }
+    }
+}
---
+++ src/main/java/com/automation/utils/AppiumUtils.java
@@ -0,0 +1,29 @@
+﻿package com.automation.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class AppiumUtils {
+
+    private static Properties properties;
+
+    // Load config.properties file
+    public static Properties getProperties() {
+        if (properties == null) {
+            properties = new Properties();
+            try {
+                FileInputStream fis = new FileInputStream("config/config.properties");
+                properties.load(fis);
+            } catch (IOException e) {
+                throw new RuntimeException("Cannot load config.properties: " + e.getMessage());
+            }
+        }
+        return properties;
+    }
+
+    // Get a specific property value
+    public static String getProperty(String key) {
+        return getProperties().getProperty(key);
+    }
+}
